b8857a96ff37befa15b3528d9b8a693081cd65ef
Prince96424 <prince96424@icloud.com>
2017-05-15 14:51:17 +0200

    Add files via upload

---
+++ Application/Project/Camping/Camping/DatabaseConnector.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Entrance
+{
+    class DatabaseConnector
+    {
+        /********* instance varible ***********/
+        private string connectionString;
+        private MySqlConnection databaseConnection;
+
+        /********* Construcor ***********/
+        public DatabaseConnector()
+        {
+            connectionString = "server=studmysql01.fhict.local;" + "Database=dbi350130;" +"Uid=dbi350130;" + "Pwd=Techrex;" + "connect timeout=30;";
+
+            databaseConnection = new MySqlConnection(connectionString);
+        }
+
+        //Open database
+        public bool IsConnected()
+        {
+            bool connected = false;
+            try
+            {
+                databaseConnection.Open();
+                connected = !connected;
+                return connected;
+            }
+            catch
+            {
+                databaseConnection.Close();
+                return connected;
+            }
+
+        }
+    }
+}
--- Application/Project/Camping/Camping/Form_Entrance.cs
+++ Application/Project/Camping/Camping/Form_Entrance.cs
@@ -7,14 +7,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Phidgets;
+using Phidgets.Events;
 
 namespace Camping
 {
     public partial class Form_Entrance : Form
     {
+        /*************** feild *****************/
+        private int RFIDNumber,NumberScanned;
+        private int numberOfPeopleOfCampingSpot = 6;
+        RFID UserRFID;
+
+
+
+        //Constrctor
         public Form_Entrance()
         {
             InitializeComponent();
+
+            UserRFID = new RFID();
+
+            try
+            {
+                UserRFID.open();
+                UserRFID.waitForAttachment(3000);
+                UserRFID.Antenna = true;
+                UserRFID.LED = true;
+            }
+            catch
+            {
+                listBox_member.Items.Clear();
+                listBox_member.Items.Add("Device connected failed");
+            }
+
+        }
+
+        /***************************  method  *********************/
+
+        public void AttachTag(object sender,TagEventArgs e)
+        {
+            
+            listBox_member.Items.Add(e.Tag.ToString());
+
+            if (listBox_member.Items.Count > numberOfPeopleOfCampingSpot) listBox_member.Items.Clear();
+        }
+
+        private void Form_Entrance_Load(object sender, EventArgs e)
+        {
+            UserRFID.Tag += new TagEventHandler(AttachTag);
+        }
+
+        public void GetRFID()
+        {
+            //turn RFID scanner
+            //Scan RFID
+            //get RFID number Contact with database
+            //check what type of spot they booked. numberOfPeopleOfCampingSpot = data from database
+        }
+
+        //Check what type of spot they booked
+        public void GetBookedNumber()
+        {
+            this.numberOfPeopleOfCampingSpot = 0;//retrive from database;
         }
     }
 }

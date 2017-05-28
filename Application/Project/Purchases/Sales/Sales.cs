a0c3ae7e51cc51860daca2f3d465f63bc7d80e8c
Prince96424 <prince96424@icloud.com>
2017-05-28 22:19:47 +0200

    purchase

--- Application/Project/Purchases/Sales/Sales.cs
+++ Application/Project/Purchases/Sales/Sales.cs
@@ -7,14 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Phidgets;
+using Phidgets.Events;
+using MySql.Data.MySqlClient;
 
 namespace Sales
 {
     public partial class Sales : Form
     {
+
+        /***************  field  *************************/
+        private int id;
+        private string rfidTag;
+        RFID UserRFID;
+
+
         public Sales()
         {
             InitializeComponent();
+
+
+            try
+            {
+                //RFID part
+                UserRFID.open();
+                UserRFID.waitForAttachment(3000);
+                UserRFID.Antenna = true;
+                UserRFID.LED = true;
+                UserRFID.Tag += new TagEventHandler(AttcahTag);
+                UserRFID.TagLost += new TagEventHandler(DetachTag);
+            }
+            catch (PhidgetException pe)
+            {
+
+            }
         }
+
+        //About Tag
+        public void AttcahTag(object sender, TagEventArgs e)
+        {
+            //method called before detach
+            rfidTag = e.Tag.ToString();
+
+        }
+        public void DetachTag(object sender, TagEventArgs e)
+        {
+            //method called after detach
+        }
+        //Above about tag
     }
 }

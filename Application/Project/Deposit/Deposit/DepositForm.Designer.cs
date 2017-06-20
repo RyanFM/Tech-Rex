92bff4b26f97f7d8c5eb0abbd8d81a2f4d646917
Ryan FM <ryanmeijs@hotmail.com>
2017-06-20 02:05:48 +0200

    Paypal deposit functionality

---
+++ Application/Project/Deposit/Deposit/DepositForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Deposit
+{
+    partial class Main_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Add = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_Add
+            // 
+            this.button_Add.Location = new System.Drawing.Point(97, 96);
+            this.button_Add.Margin = new System.Windows.Forms.Padding(4);
+            this.button_Add.Name = "button_Add";
+            this.button_Add.Size = new System.Drawing.Size(186, 119);
+            this.button_Add.TabIndex = 10;
+            this.button_Add.Text = "ADD";
+            this.button_Add.UseVisualStyleBackColor = true;
+            this.button_Add.Click += new System.EventHandler(this.button_Add_Click);
+            // 
+            // Main_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 353);
+            this.Controls.Add(this.button_Add);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Main_Form";
+            this.Text = "Deposit";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Add;
+    }
+}
+
---
+++ Application/Project/Deposit/Deposit/DepositForm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using MySql.Data.MySqlClient;
+
+namespace Deposit
+{
+    public partial class Main_Form : Form
+    {
+        public Main_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Add_Click(object sender, EventArgs e)
+        {
+            
+            string connectionString = "server=studmysql01.fhict.local;" + "Database=dbi350130;" + "Uid=dbi350130;" + "Pwd=Techrex;" + "connect timeout=30;";
+           
+
+          
+            int NrOfDeposits = 0;
+            int BankAccount = 0;
+          
+            string StartTime;
+            string EndTime;
+            FileStream fs = null;
+            StreamReader sr = null;
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            try
+            {
+               
+                databaseConnection.Open();
+                //fs = new FileStream(@"Resources\deposit.txt", FileMode.Open, FileAccess.Read);
+                //sr = new StreamReader(fs);
+
+
+                //while ((line = sr.ReadLine()) != null)
+                //{
+                string[] fields = File.ReadAllLines(@"Resources\deposit.txt");
+                   
+                   
+                        BankAccount = Convert.ToInt32(fields[0]);
+                        StartTime = fields[1];
+                        EndTime = fields[2];
+                        NrOfDeposits = Convert.ToInt32(fields[3]);
+                     
+
+
+                    for (int i = 0; i < NrOfDeposits; i++)
+                    {
+                        string query = "INSERT INTO deposit (bank_account, start_time, end_time, deposit_amount, deposit_detail) VALUES (" + BankAccount + ",'" + StartTime + "','" + EndTime + "'," + NrOfDeposits + ",'" + fields[4 + i].ToString() + "')";
+                        MySqlCommand command = new MySqlCommand(query, databaseConnection);
+                    command.ExecuteNonQuery();
+                    }
+                    
+                //}
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Error reading file");
+
+            }
+            finally
+            {
+                databaseConnection.Close();
+            }
+            
+        }
+    }
+}
